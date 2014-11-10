f73d75ec0b8aeb064c42b958c1e9c859f36c75eb
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2014-11-10 14:57:11 -0800

    updated interface based on readme

--- framework.go
+++ framework.go
@@ -3,8 +3,23 @@ package meritop
 // These two are useful for task to inform the framework their status change.
 // metaData has to be really small, since it might be stored in etcd.
 type Framework interface {
-	// SendParentStutus sends the metaData to the partent of the given task.
-	SendParentStatus(taskID uint64, metaData []byte)
-	// SendParentStutus sends the metaData to the chlidren of the given task.
-	SendChildStatus(taskID uint64, metaData []byte)
+	// SetUplinkStutus sends the metaData to the partent of the given task.
+	SetUplinkStatus(taskID uint64, metaData []byte)
+	// SetDownlinkStutus sends the metaData to the chlidren of the given task.
+	SetDownlinkStatus(taskID uint64, metaData []byte)
+
+	// These allow application developer to set the task configuration so framework
+	// implementation knows which task to invoke at each node.
+	AppendTask(task Task)
+	SetTaskConfig(taskConfig TaskConfig)
+
+	// This allow the application
+	SetTopology(topology Topology)
+
+	// After all the configure is done, driver need to call start so that all
+	// nodes will get into the event loop to run the application.
+	Start()
+
+	// Node can inform all participate task to exit.
+	Exit()
 }
