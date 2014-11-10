79e6662d69180c0cdafbac8fd9d176a676f54065
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2014-11-09 18:52:39 -0800

    add comments for topology usage

--- topology.go
+++ topology.go
@@ -1,7 +1,19 @@
+/*
+The topology is a data structure that implemented by application, and
+used by frameework implementation to setup/manage the event according to
+topology defined here. The main usage is:
+a. At beginning of the framework initialization for each task, framework
+   call the SetTaskID so that the singleton Topology knows which taskID it
+   represents.
+b. At beginning of each epock, the framework implementation (on each task)
+   will call GetParents and GetChildren with given epoch, so that it know
+   how to setup watcher for node failures. 
+*/
 package meritop
 
 // The Topology will be implemented by the application.
-// Each Topology might have many epochs. The topology of each epoch might be different.
+// Each Topology might have many epochs. The topology of each epoch 
+// might be different.
 type Topology interface {
 	// This method is called once by framework implementation. So that
 	// we can get the local topology for each epoch later.
