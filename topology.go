1384a69d7806296f5b3e03e86a3e5ea471bcb72c
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2014-11-09 18:22:48 -0800

    refactor the topology

--- topology.go
+++ topology.go
@@ -3,10 +3,15 @@ package meritop
 // The Topology will be implemented by the application.
 // Each Topology might have many epochs. The topology of each epoch might be different.
 type Topology interface {
-	// GetParents returns the parents' IDs of the given taskID at the
+	// This method is called once by framework implementation. So that
+	// we can get the local topology for each epoch later.
+	SetTaskID(taskID uint64)
+
+	// GetParents returns the parents' IDs of this task at the
 	// given epoch.
-	GetParents(epochID, taskID uint64) []uint64
-	// GetChlidren returns the children's IDs of the given taskID at the
+	GetParents(epochID uint64) []uint64
+
+	// GetChlidren returns the children's IDs of this task at the
 	// given epoch.
-	GetChildren(epochID, taskID uint64) []uint64
+	GetChildren(epochID uint64) []uint64
 }
