f9b0a1115e3fdf15830caecd5a68b29fdc01b7f5
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2014-11-11 10:17:15 -0800

    refactor TaskConfig to TaskBuilder

--- dummy_task.go
+++ dummy_task.go
@@ -4,77 +4,117 @@ This works with
 */
 package meritop
 
-type DummyTask struct {
+import (
+	"log"
+	"os"
+)
+
+type DummyMaster struct {
 	framework       Framework
 	epochID, taskID uint64
+	logger          *log.Logger
 }
 
 // This is useful to bring the task up to speed from scratch or if it recovers.
-func (t DummyTask) Init(taskID uint64, framework Framework, config Config) {
+func (t DummyMaster) Init(taskID uint64, framework Framework, config Config) {
 	t.taskID = taskID
 	t.framework = framework
+	t.logger = log.New(os.Stdout, "DummyMaster:", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 // Task need to finish up for exit, last chance to save work?
-func (t DummyTask) Exit() {}
+func (t DummyMaster) Exit() {}
 
 // These are called by framework implementation so that task implementation can
 // reacts to parent or children restart.
-func (t DummyTask) ParentRestart(parentID uint64) {}
-func (t DummyTask) ChildRestart(childID uint64)   {}
+func (t DummyMaster) ParentRestart(parentID uint64) {}
+func (t DummyMaster) ChildRestart(childID uint64)   {}
 
-func (t DummyTask) ParentDie(parentID uint64) {}
-func (t DummyTask) ChildDie(childID uint64)   {}
+func (t DummyMaster) ParentDie(parentID uint64) {}
+func (t DummyMaster) ChildDie(childID uint64)   {}
 
 // Ideally, we should also have the following:
-func (t DummyTask) ParentReady(taskID uint64, data []byte) {
-	// Get data from parent, and then make it available for children.
-	if t.framework.HasChildren() {
-		t.framework.SetReadyForChildren(nil)
-	} else {
-		t.framework.SetReadyForParent(nil)
-	}
+func (t DummyMaster) ParentReady(taskID uint64, data []byte) {
+	t.logger.Fatal("There should be no parent for master")
+	t.framework.Exit()
 }
 
-func (t DummyTask) ChildReady(taskID uint64, data []byte) {
-	// Get data from child. When all the data is back, we flag the parent.
-	if t.taskID != 0 {
-		t.framework.SetReadyForParent(nil)
-	}
+func (t DummyMaster) ChildReady(taskID uint64, data []byte) {
+	// Get data from child. When all the data is back, starts the next epoch.
 }
 
 // This give the task an opportunity to cleanup and regroup.
-func (t DummyTask) SetEpoch(epochID uint64) {
+func (t DummyMaster) SetEpoch(epochID uint64) {
 	t.epochID = epochID
-	if t.taskID == 0 {
-		// this is master node, it should set the parent ready.
-		t.framework.SetReadyForChildren(nil)
+	t.framework.FlagReadyForChildren(nil)
+}
+
+type DummySlave struct {
+	framework       Framework
+	epochID, taskID uint64
+	logger          *log.Logger
+}
+
+// This is useful to bring the task up to speed from scratch or if it recovers.
+func (t DummySlave) Init(taskID uint64, framework Framework, config Config) {
+	t.taskID = taskID
+	t.framework = framework
+	t.logger = log.New(os.Stdout, "DummySlave:", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+// Task need to finish up for exit, last chance to save work?
+func (t DummySlave) Exit() {}
+
+// These are called by framework implementation so that task implementation can
+// reacts to parent or children restart.
+func (t DummySlave) ParentRestart(parentID uint64) {}
+func (t DummySlave) ChildRestart(childID uint64)   {}
+
+func (t DummySlave) ParentDie(parentID uint64) {}
+func (t DummySlave) ChildDie(childID uint64)   {}
+
+// Ideally, we should also have the following:
+func (t DummySlave) ParentReady(taskID uint64, data []byte) {
+	// Get data from parent, and then make it available for children.
+	if t.framework.HasChildren() {
+		t.framework.FlagReadyForChildren(nil)
 	} else {
-		// this is client node, it should wait for parent ready then inform
-		// its children
+		t.framework.FlagReadyForParent(nil)
 	}
 }
 
-// Some hooks that need for master slave etc.
-func (t DummyTask) BecameMaster(nodeID uint64) {}
-func (t DummyTask) BecameSlave(nodeID uint64)  {}
+func (t DummySlave) ChildReady(taskID uint64, data []byte) {
+	// Get data from child. When all the data is back, we flag the parent.
+	t.framework.FlagReadyForParent(nil)
+}
 
-type simpleTaskConfig struct{}
+// This give the task an opportunity to cleanup and regroup.
+func (t DummySlave) SetEpoch(epochID uint64) {
+	t.epochID = epochID
+
+	// this is client node, it should wait for parent ready then inform
+	// its children
+}
+
+type simpleTaskBuilder struct{}
 
 // This method is called once by framework implementation to get the
 // right task implementation for the node/task. It requires the taskID
 // for current node, and also a global array of tasks.
-func (tc simpleTaskConfig) GetTask(taskID uint64, tasks []Task) Task {
-	return tasks[0]
+func (tc simpleTaskBuilder) GetTask(taskID uint64) Task {
+
+	if taskID == 0 {
+		return DummyMaster{}
+	} else {
+		return DummySlave{}
+	}
 }
 
 // This is used to show how to drive the network.
 func drive() {
 	var framework Framework
-	var task DummyTask
-	var taskConfig simpleTaskConfig
-	framework.AppendTask(task)
-	framework.SetTaskConfig(taskConfig)
+	var taskBuilder simpleTaskBuilder
+	framework.SetTaskBuilder(taskBuilder)
 	framework.SetTopology(NewTreeTopology(2, 127))
 	framework.Start()
 }
--- framework.go
+++ framework.go
@@ -1,17 +1,18 @@
 package meritop
 
+import "log"
+
 // These two are useful for task to inform the framework their status change.
 // metaData has to be really small, since it might be stored in etcd.
 type Framework interface {
 	// Flags and Sends the metaData to partent of the current task.
-	SetReadyForParent(metaData []byte)
+	FlagReadyForParent(metaData []byte)
 	// Flags and Sends the metaData to chlidren of the current task.
-	SetReadyForChildren(metaData []byte)
+	FlagReadyForChildren(metaData []byte)
 
 	// These allow application developer to set the task configuration so framework
 	// implementation knows which task to invoke at each node.
-	AppendTask(task Task)
-	SetTaskConfig(taskConfig TaskConfig)
+	SetTaskBuilder(taskBuilder TaskBuilder)
 
 	// This allow the application
 	SetTopology(topology Topology)
@@ -25,6 +26,8 @@ type Framework interface {
 
 	// Some task can inform all participating tasks to new epoch
 	SetEpoch(epochID uint64)
+
+	GetLogger() log.Logger
 
 	// This allow task implementation to node corresponding to taskID so that
 	// it can carry out application dependent communication.
