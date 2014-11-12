cfbc83e6d97ab3be78de29de07134660c9e7070b
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2014-11-12 10:25:42 -0800

    first reasonable full implementation of dummytask

--- dummy_task.go
+++ dummy_task.go
@@ -9,10 +9,41 @@ import (
 	"os"
 )
 
+// DummyData is used to carry parameter and gradient;
+type DummyData struct {
+	fromTaskID, toTaskID, epochID, uuID uint64
+	value                               float32
+	data                                [10]float32
+}
+
+func (d DummyData) EpochID() uint64 {
+	return d.epochID
+}
+
+func (d DummyData) ToTaskID() uint64 {
+	return d.toTaskID
+}
+
+func (d DummyData) FromTaskID() uint64 {
+	return d.fromTaskID
+}
+
+func (d DummyData) UUID() uint64 {
+	return d.uuID
+}
+
+// DummyMaster is prototype of parameter server, for now it does not
+// carry out optimization yet. But it should be easy to add support when
+// this full tests out.
+// Note: in theory, since there should be no parent of this, so we should
+// add error checing in the right places. We will skip these test for now.
 type DummyMaster struct {
 	framework       Framework
 	epochID, taskID uint64
 	logger          *log.Logger
+
+	param, gradient DummyData
+	fromChildren    map[uint64]DummyData
 }
 
 // This is useful to bring the task up to speed from scratch or if it recovers.
@@ -20,6 +51,9 @@ func (t DummyMaster) Init(taskID uint64, framework Framework, config Config) {
 	t.taskID = taskID
 	t.framework = framework
 	t.logger = log.New(os.Stdout, "DummyMaster:", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Jump start the taskgraph
+	t.framework.SetEpoch(0)
 }
 
 // Task need to finish up for exit, last chance to save work?
@@ -34,36 +68,59 @@ func (t DummyMaster) ParentDie(parentID uint64) {}
 func (t DummyMaster) ChildDie(childID uint64)   {}
 
 // Ideally, we should also have the following:
-func (t DummyMaster) ParentMetaReady(taskID uint64, meta Metadata) {
-	t.logger.Fatal("There should be no parent for master")
-	t.framework.Exit()
-}
-
+func (t DummyMaster) ParentMetaReady(taskID uint64, meta Metadata) {}
 func (t DummyMaster) ChildMetaReady(taskID uint64, meta Metadata) {
 	// Get data from child. When all the data is back, starts the next epoch.
+	t.framework.DataRequest(taskID, meta)
 }
 
 // This give the task an opportunity to cleanup and regroup.
 func (t DummyMaster) SetEpoch(epochID uint64) {
 	t.epochID = epochID
-	t.framework.FlagChildMetaReady(nil)
+	for i := 0; i < 10; i++ {
+		t.param.data[i] = float32(t.epochID)
+	}
+
+	// Make sure we have a clean slate.
+	t.fromChildren = make(map[uint64]DummyData)
+	t.framework.FlagChildMetaReady(t.param)
 }
 
 // These are payload rpc for application purpose.
-func (t DummyMaster) ServeAsParent(req Metadata) Metadata {
-	return nil
-}
-func (t DummyMaster) ServeAsChild(reg Metadata) Metadata {
-	return nil
-}
+func (t DummyMaster) ServeAsParent(req Metadata) Metadata { return t.param }
+func (t DummyMaster) ServeAsChild(reg Metadata) Metadata  { return nil }
 
 func (t DummyMaster) ParentDataReady(req, response Metadata) {}
-func (t DummyMaster) ChildDataReady(req, response Metadata)  {}
+func (t DummyMaster) ChildDataReady(req, response Metadata) {
+	if req.EpochID() != t.epochID {
+		return
+	}
 
+	data, ok := req.(DummyData)
+	if !ok {
+		t.logger.Fatal("Can't interpret request")
+	}
+	t.fromChildren[data.FromTaskID()] = data
+
+	// This is a weak form of checking. We can also check the task ids.
+	// But this really means that we get all the events from children, we
+	// should go into the next epoch now.
+	if len(t.fromChildren) == len(t.framework.GetTopology().GetChildren(t.epochID)) {
+		// In real ML, we modify the gradient first. But here it is noop.
+		t.framework.SetEpoch(t.epochID + 1)
+	}
+}
+
+// DummySlave is an prototype for data shard in machine learning applications.
+// It mainly does to things, pass on parameters to its children, and collect
+// gradient back then add them together before make it available to its parent.
 type DummySlave struct {
 	framework       Framework
 	epochID, taskID uint64
 	logger          *log.Logger
+
+	param, gradient DummyData
+	fromChildren    map[uint64]DummyData
 }
 
 // This is useful to bring the task up to speed from scratch or if it recovers.
@@ -86,37 +143,80 @@ func (t DummySlave) ChildDie(childID uint64)   {}
 
 // Ideally, we should also have the following:
 func (t DummySlave) ParentMetaReady(taskID uint64, meta Metadata) {
-	// Get data from parent, and then make it available for children.
-	if t.framework.HasChildren() {
-		t.framework.FlagChildMetaReady(nil)
-	} else {
-		t.framework.FlagParentMetaReady(nil)
-	}
+	t.framework.DataRequest(taskID, meta)
 }
 
 func (t DummySlave) ChildMetaReady(taskID uint64, meta Metadata) {
-	// Get data from child. When all the data is back, we flag the parent.
-	t.framework.FlagParentMetaReady(nil)
+	t.framework.DataRequest(taskID, meta)
 }
 
 // This give the task an opportunity to cleanup and regroup.
 func (t DummySlave) SetEpoch(epochID uint64) {
 	t.epochID = epochID
 
-	// this is client node, it should wait for parent ready then inform
-	// its children
+	// Make sure we have a clean slate.
+	t.fromChildren = make(map[uint64]DummyData)
 }
 
 // These are payload rpc for application purpose.
 func (t DummySlave) ServeAsParent(req Metadata) Metadata {
-	return nil
+	return t.param
 }
 func (t DummySlave) ServeAsChild(reg Metadata) Metadata {
-	return nil
+	return t.gradient
 }
 
-func (t DummySlave) ParentDataReady(req, response Metadata) {}
-func (t DummySlave) ChildDataReady(req, response Metadata)  {}
+func (t DummySlave) ParentDataReady(req, response Metadata) {
+	if req.EpochID() != t.epochID {
+		return
+	}
+
+	data, ok := req.(DummyData)
+	if !ok {
+		t.logger.Fatal("Can't interpret request")
+	}
+	t.param = data
+
+	// We need to carry out local compuation.
+	for i := 0; i < 10; i++ {
+		t.gradient.data[i] = float32(t.framework.GetTaskID())
+	}
+
+	// If this task has children, flag meta so that children can start pull
+	// parameter.
+	if t.framework.HasChildren() {
+		t.framework.FlagChildMetaReady(t.param)
+	} else {
+		// On leaf node, we can immediately return by and flag parent
+		// that this node is ready.
+		t.framework.FlagParentMetaReady(t.gradient)
+	}
+}
+
+func (t DummySlave) ChildDataReady(req, response Metadata) {
+	if req.EpochID() != t.epochID {
+		return
+	}
+	data, ok := req.(DummyData)
+	if !ok {
+		t.logger.Fatal("Can't interpret request")
+	}
+	t.fromChildren[data.FromTaskID()] = data
+
+	// This is a weak form of checking. We can also check the task ids.
+	// But this really means that we get all the events from children, we
+	// should go into the next epoch now.
+	if len(t.fromChildren) == len(t.framework.GetTopology().GetChildren(t.epochID)) {
+		// In real ML, we add the gradient first.
+		for _, g := range t.fromChildren {
+			for i := 0; i < 10; i++ {
+				t.gradient.data[i] += g.data[i]
+			}
+		}
+
+		t.framework.FlagParentMetaReady(t.gradient)
+	}
+}
 
 type simpleTaskBuilder struct{}
 
@@ -133,7 +233,7 @@ func (tc simpleTaskBuilder) GetTask(taskID uint64) Task {
 
 // This is used to show how to drive the network.
 func drive() {
-	var framework Framework
+	var framework FrameworkInit
 	var taskBuilder simpleTaskBuilder
 	framework.SetTaskBuilder(taskBuilder)
 	framework.SetTopology(NewTreeTopology(2, 127))
