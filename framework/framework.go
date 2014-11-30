91f81cda25e8072d789c02bb4142b29114de7237
Xiang Li <xiang.li@coreos.com>
2014-11-29 17:05:50 -0800

    framework: move funcs

--- framework/framework.go
+++ framework/framework.go
@@ -76,17 +76,6 @@ func (f *framework) parentOrChild(taskID uint64) taskRole {
 	return roleNone
 }
 
-func (f *framework) fetchEpoch() (uint64, error) {
-	f.etcdClient = etcd.NewClient(f.etcdURLs)
-
-	epochPath := etcdutil.MakeJobEpochPath(f.name)
-	resp, err := f.etcdClient.Get(epochPath, false, false)
-	if err != nil {
-		f.log.Fatal("Can not get epoch from etcd")
-	}
-	return strconv.ParseUint(resp.Node.Value, 10, 64)
-}
-
 // occupyTask will grab the first unassigned task and register itself on etcd.
 func (f *framework) occupyTask() (uint64, error) {
 	// get all nodes under task dir
@@ -113,18 +102,6 @@ func (f *framework) occupyTask() (uint64, error) {
 		}
 	}
 	return 0, fmt.Errorf("no unassigned task found")
-}
-
-// Framework http server for data request.
-// Each request will be in the format: "/datareq?taskID=XXX&req=XXX".
-// "taskID" indicates the requesting task. "req" is the meta data for this request.
-// On success, it should respond with requested data in http body.
-func (f *framework) startHTTP() {
-	f.log.Printf("serving http on %s", f.ln.Addr())
-	// TODO: http server graceful shutdown
-	if err := http.Serve(f.ln, &dataReqHandler{f}); err != nil {
-		f.log.Fatalf("http.Serve() returns error: %v\n", err)
-	}
 }
 
 // Framework event loop handles data response for requests sent in DataRequest().
@@ -168,63 +145,6 @@ func (f *framework) IncEpoch() {
 	if err != nil {
 		f.log.Fatalf("Epoch CompareAndSwap(%d, %d) failed: %v", f.epoch+1, f.epoch, err)
 	}
-}
-
-func (f *framework) watchEpoch() {
-	receiver := make(chan *etcd.Response, 1)
-	f.epochChan = make(chan uint64, 1)
-	f.epochStop = make(chan bool, 1)
-
-	watchPath := etcdutil.MakeJobEpochPath(f.name)
-	go f.etcdClient.Watch(watchPath, 1, false, receiver, f.epochStop)
-	go func(receiver <-chan *etcd.Response) {
-		for resp := range receiver {
-			if resp.Action != "compareAndSwap" && resp.Action != "set" {
-				continue
-			}
-			epoch, err := strconv.ParseUint(resp.Node.Value, 10, 64)
-			if err != nil {
-				f.log.Fatal("Can't parse epoch from etcd")
-			}
-			f.epochChan <- epoch
-		}
-	}(receiver)
-}
-
-func (f *framework) watchAll(who taskRole, taskIDs []uint64) {
-	stops := make([]chan bool, len(taskIDs))
-
-	for i, taskID := range taskIDs {
-		receiver := make(chan *etcd.Response, 10)
-		stop := make(chan bool, 1)
-		stops[i] = stop
-
-		var watchPath string
-		var taskCallback func(uint64, string)
-		switch who {
-		case roleParent:
-			// Watch parent's child.
-			watchPath = etcdutil.MakeChildMetaPath(f.name, taskID)
-			taskCallback = f.task.ParentMetaReady
-		case roleChild:
-			// Watch child's parent.
-			watchPath = etcdutil.MakeParentMetaPath(f.name, taskID)
-			taskCallback = f.task.ChildMetaReady
-		default:
-			panic("unimplemented")
-		}
-
-		go f.etcdClient.Watch(watchPath, 1, false, receiver, stop)
-		go func(receiver <-chan *etcd.Response, taskID uint64) {
-			for resp := range receiver {
-				if resp.Action != "set" {
-					continue
-				}
-				taskCallback(taskID, resp.Node.Value)
-			}
-		}(receiver, taskID)
-	}
-	f.stops = append(f.stops, stops...)
 }
 
 // getAddress will return the host:port address of the service taking care of
