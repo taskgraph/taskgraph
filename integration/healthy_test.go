7aad62acaf039fdfe5999bc710bd3d284c39d7dc
Hongchao Deng <hdeng@cloudera.com>
2014-11-30 18:42:05 -0800

    move healthy_test to integration/

--- integration/healthy_test.go
+++ integration/healthy_test.go
@@ -1,15 +1,16 @@
-package etcdutil
+package integration
 
 import (
 	"testing"
 	"time"
 
 	"github.com/coreos/go-etcd/etcd"
+	"github.com/go-distributed/meritop/pkg/etcdutil"
 )
 
 func TestHeartbeat(t *testing.T) {
 	name := "TestHeartbeat"
-	m := StartNewEtcdServer(t, name)
+	m := etcdutil.StartNewEtcdServer(t, name)
 	defer m.Terminate(t)
 
 	client := etcd.NewClient([]string{m.URL()})
@@ -18,24 +19,24 @@ func TestHeartbeat(t *testing.T) {
 	interval := time.Duration(ttl) * time.Second
 	stop := make(chan struct{}, 1)
 
-	client.Create(HealthyPath(name, taskID), "health", ttl)
+	client.Create(etcdutil.HealthyPath(name, taskID), "health", ttl)
 	time.Sleep(2 * interval)
-	_, err := client.Get(HealthyPath(name, taskID), false, false)
+	_, err := client.Get(etcdutil.HealthyPath(name, taskID), false, false)
 	if err == nil {
 		t.Fatal("ttl node should expire")
 	}
 
-	client.Create(HealthyPath(name, taskID), "health", ttl)
-	go Heartbeat(client, name, taskID, interval, stop)
+	client.Create(etcdutil.HealthyPath(name, taskID), "health", ttl)
+	go etcdutil.Heartbeat(client, name, taskID, interval, stop)
 	time.Sleep(10 * interval)
-	_, err = client.Get(HealthyPath(name, taskID), false, false)
+	_, err = client.Get(etcdutil.HealthyPath(name, taskID), false, false)
 	if err != nil {
 		t.Fatalf("client.Get failed: %v", err)
 	}
 
 	close(stop)
 	time.Sleep(10 * interval)
-	_, err = client.Get(HealthyPath(name, taskID), false, false)
+	_, err = client.Get(etcdutil.HealthyPath(name, taskID), false, false)
 	if err == nil {
 		t.Fatal("ttl node should expire")
 	}
