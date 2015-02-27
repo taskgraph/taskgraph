166b255d8b91d3cd71ad74d9b60c17c512067f2f
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2015-02-27 14:36:30 -0800

    refactored the func interface so that we can iterate through all indexes

--- func_interface.go
+++ func_interface.go
@@ -1,22 +1,23 @@
 package taskgraph
 
+// For more information read: http://ewencp.org/blog/golang-iterators/
+// We need an reasonably efficient way to enumerate all indexes.
+type IndexIterator interface {
+	Index() uint64
+    Next() bool
+}
+
 // We need some interface to define function and how we optimize them.
 type Parameter interface {
 	GetParam(index uint64) float32
 	SetParam(index uint64, value float32)
-}
-
-type Gradient interface {
-	GetValue() float32
-	GetGradient(index uint64) float32
-	SetValue(value float32)
-	SetGradient(index uint64, grad float32)
+	GetIndexInterator() IndexIterator
 }
 
 // There are many different ways one can optimize a function, but the
 // most effective ones need gradient.
 type Function interface {
-	Evaluate(param Parameter) Gradient
+	Evaluate(param Parameter) (value float32, gradient Parameter)
 }
 
 // High level interface for minimization. This assume that we start
