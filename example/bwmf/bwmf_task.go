ed354aa77de14c818f20eaa07a3b27c7882f55f1
Gang Bai <me@baigang.net>
2015-03-01 17:26:26 +0800

    Cleaned code for originally matrix-op based solution.

--- example/bwmf/bwmf_task.go
+++ example/bwmf/bwmf_task.go
@@ -77,186 +77,59 @@ type bwmfTask struct {
 	d, t           Shard
 
 	// parameters for projected gradient methods
-	sigma, alpha, beta    float64
-	maxIterInSubProblem   int
-	maxIterInFindingAlpha int
+	sigma, alpha, beta float64
+
+	// objective function, parameters and minimizer to solve bwmf
+	// TODO
 }
 
 // These two function carry out actual optimization.
-func (t *bwmfTask) updateDShard() {
-	var newDShard Shard = t.dShard
-	var ok bool
-	for iter := 0; iter < t.maxIterInSubProblem; iter++ {
-		newDShard, ok = t.solveSubproblem(&t.rowShard, &newDShard, &t.t)
-		// TODO(baigang): dealing with ok==false
-		// TODO(baigang): logging
-	}
-	t.dShard = newDShard
-}
-func (t *bwmfTask) updateTShard() {
-	var newTShard Shard = t.tShard
-	var ok bool
-	for iter := 0; iter < t.maxIterInSubProblem; iter++ {
-		newTShard, ok = t.solveSubproblem(&t.columnShard, &t.tShard, &t.d)
-		// TODO(baigang): dealing with ok==false
-		// TODO(baigang): logging
-	}
-	t.tShard = newTShard
+func (this *bwmfTask) updateDShard() {
 }
 
-// calculates $matrix^T * matrix$
-func matrixSelfTransMult(matrix *Shard) Shard {
-	var result Shard
-	// TODO(baigang): implementation
-	// length := len(matrix[0])
-	return result
-}
-
-// calculates $a * b$ where $b$ is a sparseVec
-func matrixSparseMult(a *Shard, b *[]sparseVec) Shard {
-	var result Shard
-	// TODO(baigang): implementation
-	return result
-}
-
-// caculates $a * b$
-func matrixMult(a, b *Shard) Shard {
-	var result Shard
-	// TODO(baigang): implementation
-	return result
-}
-
-// caculates $a - b$
-func matrixSub(a, b *Shard) Shard {
-	var result Shard
-	// TODO(baigang): implementation
-	return result
-}
-
-// returns true is a equals b, as \|a - \|b_F^2 < epsilon
-func matrixEqual(a, b *Shard) bool {
-	// TODO(baigang): implementation
-	return true
-}
-
-// calculates the sum of the component-wise product of matrx $\mathbf{a}$ and $\mathbf{b}$
-func calcInProduct(a, b *Shard) float64 {
-	// TODO(baigang): implementation
-	return 1.0
-}
-
-// for each element in matrix $\mathbf{t} - alpha * \mathbf{g}$, do
-//    elem = elem if elem > l && elem < u,
-//           l  if elem <= l,
-//           u  if elem >= u
-func doProjection(t, g *Shard, alpha, l, u float64) Shard {
-	result := make([]*bwmfData, len(*t))
-	length := len((*t)[0].Values)
-	for i, row := range *t {
-		result[i].Values = make([]float64, length)
-		for j, tValue := range (*t)[i].Values {
-			gValue := alpha * (*g)[i].Values[j]
-			value := tValue - gValue
-			if value <= l {
-				result[i].Values[j] = l
-			} else if value >= u {
-				result[i].Values[j] = u
-			}
-		}
-	}
-	return result
-}
-
-// Solve the problem: $argmin_t \| a - D*t \|_F^2$
-// where $a$ is a column shard of $A$, $D$ is a full copy of factor D, and
-// $t$ is a shard of factor T corresponding to $a$.
-//
-// XXX(baigang): This is currently an awkward translation of Matlab code listed in Appendix B.2 in ``Projected
-// Gradient Methods for Non-negative Matrix Factorization'' with a renaming convention that V is A, W is D and
-// H is tShard. Note this deals only the inner iter.
-// TODO(baigang): stopping criteria of the outer iterations. Maybe also return norm of the gradient, i.e
-// `projgrad` in B.2.
-// TODO(baigang): Fine abstraction of shard data and matrix operations.
-//
-// A is an l-sized shard of the full matrix A, so its size is m*l, D is m*k, t is k*l.
-func (t *bwmfTask) solveSubproblem(A *[]sparseVec, D, tShard *Shard) (Shard, bool) {
-	DTD := matrixSelfTransMult(D)
-	DTDt := matrixMult(&DTD, tShard)
-	DTa := matrixSparseMult(D, A)
-
-	gradient := matrixSub(&DTDt, &DTa)
-	alpha := t.alpha
-	decreaseAlpha := false
-	// Step 2 in Algorithm 4, section 3.
-	for iter := 0; iter <= t.maxIterInFindingAlpha; iter++ {
-		tShardNew := doProjection(tShard, &gradient, t.alpha, 0.0, 1e20)
-		tDiff := matrixSub(&tShardNew, tShard)
-		DTDd := matrixMult(&DTD, &tDiff)
-
-		// formula (17)
-		conditionValue := (1.0-t.sigma)*calcInProduct(&gradient, &tDiff) + 0.5*calcInProduct(&tDiff, &DTDd)
-
-		sufficientDecrese := conditionValue <= 0.0
-		if iter == 0 {
-			decreaseAlpha = !sufficientDecrese
-		}
-		// basically substep (b) in step 2, algorithm 4
-		if decreaseAlpha {
-			if sufficientDecrese {
-				return tShardNew, true
-			} else {
-				alpha = alpha * t.beta
-			}
-		} else {
-			if !sufficientDecrese || matrixEqual(&tShardNew, tShard) {
-				return tShardNew, false
-			} else {
-				alpha = alpha / t.beta
-				*tShard = tShardNew
-			}
-		}
-	}
-	return *tShard, false
+func (this *bwmfTask) updateTShard() {
 }
 
 // Initialization: We need to read row and column shards of A.
-func (t *bwmfTask) readShardsFromDisk() {}
+func (this *bwmfTask) readShardsFromDisk() {}
 
 // Read dShard and tShard from last checkpoint if any.
-func (t *bwmfTask) readLastCheckpoint() bool {
+func (this *bwmfTask) readLastCheckpoint() bool {
 	panic("")
 }
 
 // Task have all the data, compute local optimization of D/T.
-func (t *bwmfTask) localCompute() {}
+func (this *bwmfTask) localCompute() {
+
+}
 
 // This is useful to bring the task up to speed from scratch or if it recovers.
-func (t *bwmfTask) Init(taskID uint64, framework taskgraph.Framework) {
-	t.taskID = taskID
-	t.framework = framework
-	t.logger = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
+func (this *bwmfTask) Init(taskID uint64, framework taskgraph.Framework) {
+	this.taskID = taskID
+	this.framework = framework
+	this.logger = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
 	// Use some unique identifier to set Index "in the future".
 	// We can use taskID now.
-	t.dShard = newDShard(t.taskID)
-	t.tShard = newTShard(t.taskID)
-	t.readShardsFromDisk()
-	ok := t.readLastCheckpoint()
+	this.dShard = newDShard(this.taskID)
+	this.tShard = newTShard(this.taskID)
+	this.readShardsFromDisk()
+	ok := this.readLastCheckpoint()
 	if !ok {
-		t.dShard.randomFillValue()
-		t.tShard.randomFillValue()
+		this.dShard.randomFillValue()
+		this.tShard.randomFillValue()
 	}
 
 	// At initialization:
 	// Task 0 will start the iterations.
 }
 
-func (t *bwmfTask) Exit() {}
+func (this *bwmfTask) Exit() {}
 
-func (t *bwmfTask) SetEpoch(ctx taskgraph.Context, epoch uint64) {
-	t.logger.Printf("slave SetEpoch, task: %d, epoch: %d\n", t.taskID, epoch)
-	t.epoch = epoch
-	t.childrenReady = make(map[uint64]bool)
-	t.dtReady = common.NewCountdownLatch(int(t.numOfTasks))
+func (this *bwmfTask) SetEpoch(ctx taskgraph.Context, epoch uint64) {
+	this.logger.Printf("slave SetEpoch, task: %d, epoch: %d\n", this.taskID, epoch)
+	this.epoch = epoch
+	this.childrenReady = make(map[uint64]bool)
+	this.dtReady = common.NewCountdownLatch(int(this.numOfTasks))
 
 	// Afterwards:
 	// We need to get all D/T from last epoch so that we can carry out local
@@ -264,36 +137,37 @@ func (t *bwmfTask) SetEpoch(ctx taskgraph.Context, epoch uint64) {
 	// Even epochs: Fix D, calculate T;
 	// Odd epochs: Fix T, calculate D;
 
-	if t.epoch%2 == 0 {
-		for index := uint64(0); index < t.numOfTasks; index++ {
+	if this.epoch%2 == 0 {
+		for index := uint64(0); index < this.numOfTasks; index++ {
 			ctx.DataRequest(index, "getD")
 		}
 	} else {
-		for index := uint64(0); index < t.numOfTasks; index++ {
+		for index := uint64(0); index < this.numOfTasks; index++ {
 			ctx.DataRequest(index, "getT")
 		}
 	}
 
 	go func() {
 		// Wait for all shards (either D or T, depending on the epoch) to be ready.
-		t.dtReady.Await()
+		this.dtReady.Await()
 		// We can compute local shard result from A and D/T.
-		t.localCompute()
+		this.localCompute()
 		// Notify task 0 about the result.
 		ctx.FlagMetaToParent("computed")
 	}()
 }
 
-func (t *bwmfTask) ParentMetaReady(ctx taskgraph.Context, parentID uint64, meta string) {}
+func (this *bwmfTask) ParentMetaReady(ctx taskgraph.Context, parentID uint64, meta string) {}
 
-func (t *bwmfTask) ParentDataReady(ctx taskgraph.Context, parentID uint64, req string, resp []byte) {}
+func (this *bwmfTask) ParentDataReady(ctx taskgraph.Context, parentID uint64, req string, resp []byte) {
+}
 
-func (t *bwmfTask) ChildMetaReady(ctx taskgraph.Context, childID uint64, meta string) {
+func (this *bwmfTask) ChildMetaReady(ctx taskgraph.Context, childID uint64, meta string) {
 	// Task zero should maintain the barrier for iterations.
 	if meta == "computed" {
-		t.childrenReady[childID] = true
+		this.childrenReady[childID] = true
 	}
-	if uint64(len(t.childrenReady)) < t.numOfTasks {
+	if uint64(len(this.childrenReady)) < this.numOfTasks {
 		return
 	}
 	// if we have all data, start next iteration.
@@ -301,32 +175,32 @@ func (t *bwmfTask) ChildMetaReady(ctx taskgraph.Context, childID uint64, meta st
 }
 
 // Other nodes has served with their local shards.
-func (t *bwmfTask) ChildDataReady(ctx taskgraph.Context, childID uint64, req string, resp []byte) {
-	t.dtReady.CountDown()
+func (this *bwmfTask) ChildDataReady(ctx taskgraph.Context, childID uint64, req string, resp []byte) {
+	this.dtReady.CountDown()
 }
 
 // get request of D/T shards from others. Serve with local shard.
-func (t *bwmfTask) ServeAsParent(fromID uint64, req string, dataReceiver chan<- []byte) {
+func (this *bwmfTask) ServeAsParent(fromID uint64, req string, dataReceiver chan<- []byte) {
 	var b []byte
 	var err error
 
 	go func() {
-		if t.epoch%2 == 0 {
-			b, err = json.Marshal(t.dShard)
+		if this.epoch%2 == 0 {
+			b, err = json.Marshal(this.dShard)
 			if err != nil {
-				t.logger.Fatalf("Slave can't encode dShard error: %v\n", err)
+				this.logger.Fatalf("Slave can't encode dShard error: %v\n", err)
 			}
 		} else {
-			b, err = json.Marshal(t.tShard)
+			b, err = json.Marshal(this.tShard)
 			if err != nil {
-				t.logger.Fatalf("Slave can't encode tShard error: %v\n", err)
+				this.logger.Fatalf("Slave can't encode tShard error: %v\n", err)
 			}
 		}
 		dataReceiver <- b
 	}()
 }
 
-func (t *bwmfTask) ServeAsChild(fromID uint64, req string, dataReceiver chan<- []byte) {}
+func (this *bwmfTask) ServeAsChild(fromID uint64, req string, dataReceiver chan<- []byte) {}
 
 type BWMFTaskBuilder struct {
 }
