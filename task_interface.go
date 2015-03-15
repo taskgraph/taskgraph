f88b8ff7c4c994a45d7dd6b08a273a33d22ecc5d
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2015-03-15 12:59:44 +0800

    remove traces of XMetaReady

--- task_interface.go
+++ task_interface.go
@@ -20,10 +20,7 @@ type Task interface {
 
 	// The meta/data notifications obey exactly-once semantics. Note that the same
 	// meta string will be notified only once even if you flag the meta more than once.
-	// ParentMetaReady(ctx Context, parentID uint64, meta string)
-	// ChildMetaReady(ctx Context, childID uint64, meta string)
-
-	// This now allow use to use arbirary type instead of Parents/Children.
+	// TODO: one can also get this from channel.
 	MetaReady(ctx context.Context, childID uint64, linkType, meta string)
 
 	// These two should go away, folding into DataRequest.
