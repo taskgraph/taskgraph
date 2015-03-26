5061d3b49d97ec594b5a6f2d663b1eec289d3044
Gang Bai <me@baigang.net>
2015-03-26 17:05:19 +0800

    Added dense/sparse encapsulation of matrix shard.

--- example/bwmf/proto/bwmf.pb.go
+++ example/bwmf/proto/bwmf.pb.go
@@ -11,8 +11,8 @@ It is generated from these files:
 It has these top-level messages:
 	Request
 	Response
-	MatrixShard
-	Row
+	DenseMatrixShard
+	SparseMatrixShard
 */
 package proto
 
@@ -32,47 +32,72 @@ func (*Request) ProtoMessage()    {}
 
 // Response of the block matrix data, with the associated block id.
 type Response struct {
-	Id    int32        `protobuf:"varint,1,opt,name=id" json:"id,omitempty"`
-	Shard *MatrixShard `protobuf:"bytes,2,opt,name=shard" json:"shard,omitempty"`
+	Id    int32             `protobuf:"varint,1,opt,name=id" json:"id,omitempty"`
+	Shard *DenseMatrixShard `protobuf:"bytes,2,opt,name=shard" json:"shard,omitempty"`
 }
 
 func (m *Response) Reset()         { *m = Response{} }
 func (m *Response) String() string { return proto1.CompactTextString(m) }
 func (*Response) ProtoMessage()    {}
 
-func (m *Response) GetShard() *MatrixShard {
+func (m *Response) GetShard() *DenseMatrixShard {
 	if m != nil {
 		return m.Shard
 	}
 	return nil
 }
 
-// sharded matrix data
-// with row and Row, accessing an element will be `mat.Row[i].At[j]`
-type MatrixShard struct {
-	Row []*Row `protobuf:"bytes,1,rep,name=row" json:"row,omitempty"`
+// sharded dense matrix data
+// In bwmf, this message is for full D/T and sharded D/T data blocks.
+type DenseMatrixShard struct {
+	Row []*DenseMatrixShard_DenseRow `protobuf:"bytes,1,rep,name=row" json:"row,omitempty"`
 }
 
-func (m *MatrixShard) Reset()         { *m = MatrixShard{} }
-func (m *MatrixShard) String() string { return proto1.CompactTextString(m) }
-func (*MatrixShard) ProtoMessage()    {}
+func (m *DenseMatrixShard) Reset()         { *m = DenseMatrixShard{} }
+func (m *DenseMatrixShard) String() string { return proto1.CompactTextString(m) }
+func (*DenseMatrixShard) ProtoMessage()    {}
 
-func (m *MatrixShard) GetRow() []*Row {
+func (m *DenseMatrixShard) GetRow() []*DenseMatrixShard_DenseRow {
 	if m != nil {
 		return m.Row
 	}
 	return nil
 }
 
-type Row struct {
+type DenseMatrixShard_DenseRow struct {
+	At []float32 `protobuf:"fixed32,1,rep,name=at" json:"at,omitempty"`
+}
+
+func (m *DenseMatrixShard_DenseRow) Reset()         { *m = DenseMatrixShard_DenseRow{} }
+func (m *DenseMatrixShard_DenseRow) String() string { return proto1.CompactTextString(m) }
+func (*DenseMatrixShard_DenseRow) ProtoMessage()    {}
+
+// sharded matrix data
+// In bwmf, this message is for sharded row/column data.
+type SparseMatrixShard struct {
+	Row []*SparseMatrixShard_SparseRow `protobuf:"bytes,1,rep,name=row" json:"row,omitempty"`
+}
+
+func (m *SparseMatrixShard) Reset()         { *m = SparseMatrixShard{} }
+func (m *SparseMatrixShard) String() string { return proto1.CompactTextString(m) }
+func (*SparseMatrixShard) ProtoMessage()    {}
+
+func (m *SparseMatrixShard) GetRow() []*SparseMatrixShard_SparseRow {
+	if m != nil {
+		return m.Row
+	}
+	return nil
+}
+
+type SparseMatrixShard_SparseRow struct {
 	At map[uint32]float32 `protobuf:"bytes,1,rep,name=at" json:"at,omitempty" protobuf_key:"varint,1,opt,name=key" protobuf_val:"fixed32,2,opt,name=value"`
 }
 
-func (m *Row) Reset()         { *m = Row{} }
-func (m *Row) String() string { return proto1.CompactTextString(m) }
-func (*Row) ProtoMessage()    {}
+func (m *SparseMatrixShard_SparseRow) Reset()         { *m = SparseMatrixShard_SparseRow{} }
+func (m *SparseMatrixShard_SparseRow) String() string { return proto1.CompactTextString(m) }
+func (*SparseMatrixShard_SparseRow) ProtoMessage()    {}
 
-func (m *Row) GetAt() map[uint32]float32 {
+func (m *SparseMatrixShard_SparseRow) GetAt() map[uint32]float32 {
 	if m != nil {
 		return m.At
 	}
