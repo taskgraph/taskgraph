467ed7eface26893c6344c40a300d3c15587b575
Gang Bai <me@baigang.net>
2015-04-02 16:48:32 +0800

    Fixed script for e2e test.

--- example/bwmf/demo/run_demo.sh
+++ example/bwmf/demo/run_demo.sh
@@ -4,20 +4,20 @@ ETCDBIN=$GOPATH/etcd-v2.0.0-darwin-amd64
 RESULT_FILE="result.txt"
 
 # clear etcd
-$ETCDBIN/etcdctl rm --recursive /
-rm $RESULT_FILE
+#$ETCDBIN/etcdctl rm --recursive /
+rm -f $RESULT_FILE
 
-./regression -job="bwmf test" -type=c
+./demo -job="bwmf_test" -type=c
 
 
 # need to wait for controller to setup
 sleep 1
-./regression -job="bwmf test" -type=t -row_file=dummy -column_file=dummy \
+./demo -job="bwmf_test" -type=t -row_file=dummy -column_file=dummy \
     -latent_dim=2 -block_id=0 -num_iters=10 -alpha=0.5 -beta=0.1 -sigma=0.01 \
     -tolerance=0.0001 -etcd_urls="http://localhost:4001/v2/" \
     -num_tasks=2 &
 
-./regression -job="bwmf test" -type=t  -row_file=dummy -column_file=dummy \
+./demo -job="bwmf_test" -type=t  -row_file=dummy -column_file=dummy \
     -latent_dim=2 -block_id=1 -num_iters=10 -alpha=0.5 -beta=0.1 -sigma=0.01 \
     -tolerance=0.0001 -etcd_urls="http://localhost:4001/v2/" \
     -num_tasks=2 &
