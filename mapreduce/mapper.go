1f83eb29c67d9150dbcb1c6c8ee44fe4b107dcb6
plutoshe <plutoshe@gmail.com>
2015-04-06 18:07:04 +0800

    MapReduce : Get start

---
+++ mapreduce/mapper.go
@@ -0,0 +1,9 @@
+struct mapperTask {
+	framework taskgraph.framework
+	epoch uint64
+	log *log.logger
+	taskId uint64
+	numOfTasks uint64
+
+}
+
---
+++ mapreduce/reducer.go
@@ -0,0 +1,9 @@
+struct reducerTask {
+	framework taskgraph.framework
+	epoch uint64
+	log *log.logger
+	taskId uint64
+	numOfTasks uint64
+
+}
+
