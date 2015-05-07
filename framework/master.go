4e39bcca83dfc4d7997e1da4ffa4925e842f0c87
Hongchao Deng <hdeng@cloudera.com>
2015-05-07 12:01:19 -0700

    master Start draft

--- framework/master.go
+++ framework/master.go
@@ -4,16 +4,18 @@ import (
 	"log"
 	"net"
 
+	"github.com/coreos/go-etcd/etcd"
 	"github.com/taskgraph/taskgraph"
 )
 
 type master struct {
-	job       string
-	etcdURL   []string
-	listener  net.Listener
-	logger    *log.Logger
-	task      taskgraph.MasterTask
-	workerNum uint64
+	job        string
+	etcdURL    []string
+	listener   net.Listener
+	logger     *log.Logger
+	task       taskgraph.MasterTask
+	workerNum  uint64
+	etcdClient *etcd.Client
 }
 
 func NewMasterBoot(job string, etcdURL []string, ln net.Listener, logger *log.Logger, task taskgraph.MasterTask, workerNum uint64) taskgraph.Bootup {
--- framework/master_boot.go
+++ framework/master_boot.go
@@ -1,15 +1,47 @@
 package framework
 
-import "golang.org/x/net/context"
+import (
+	"github.com/coreos/go-etcd/etcd"
+	"golang.org/x/net/context"
+)
 
-// Master is guaranteed the first to run. It does the following:
-// - start a (etcd) controller.
-// - set up server to be listening
-// - set up message handling
-// - run user task
+// Master is guaranteed the first to run. It mainly does the following:
+// - initialize internal structures.
+// - set up etcd. Register master address. Set up layout.
+// - start server. This is the user defined grpc server.
+// - start event handling. Select and handle events, e.g. sending and receiving messages,
+//   finishing job, etc. are events.
+// - run user task.
 
 func (m *master) Start() {
-	//
+	m.init()
+	m.setupEtcd()
+	m.startServer()
+	go m.startEventHandling()
+	m.runUserTask()
+}
+
+func (m *master) init() {
+	m.etcdClient = etcd.NewClient(m.etcdURL)
+}
+
+func (m *master) setupEtcd() {
+	// init layout
+	// register address for master
+}
+
+func (m *master) startServer() {
+	s := m.task.CreateServer()
+	go s.Serve(m.listener)
+}
+
+func (m *master) startEventHandling() {
+	for {
+		select {}
+	}
+}
+
+func (m *master) runUserTask() {
 	m.task.Setup(m)
 	m.task.Run(context.Background())
 }
