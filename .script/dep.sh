7dc0948be70d22764e8b9813120cd99dc4d7b91e
plutoshe <plutoshe@gmail.com>
2015-05-14 16:29:44 +0800

    azurefilesystemUpdate : update deps.sh

--- .script/dep.sh
+++ .script/dep.sh
@@ -8,5 +8,6 @@ go get -u github.com/colinmarc/hdfs
 go get -u golang.org/x/net/context
 go get -u google.golang.org/grpc
 go get -u github.com/golang/protobuf/proto
-go get -d github.com/Azure/azure-sdk-for-go
+go get -u github.com/Azure/azure-sdk-for-go
+
 
