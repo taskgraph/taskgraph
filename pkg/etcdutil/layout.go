82f0c3652d1f407b22c1508249ec91bbc4dd9975
plutoshe <plutoshe@gmail.com>
2015-05-26 17:24:53 +0800

    mapreduce : update etcd layout
    add identifier to describe worker status in etcdutil/layout.go

--- pkg/etcdutil/layout.go
+++ pkg/etcdutil/layout.go
@@ -21,16 +21,17 @@ import (
 // /{job}/worker/{workerID}
 
 const (
-	TasksDir   = "tasks"
-	NodesDir   = "nodes"
-	ConfigDir  = "config"
-	FreeDir    = "freeTasks"
-	Epoch      = "epoch"
-	Status     = "status"
-	TaskMaster = "0"
-	NodeAddr   = "address"
-	NodeTTL    = "ttl"
-	Healthy    = "healthy"
+	TasksDir                 = "tasks"
+	NodesDir                 = "nodes"
+	ConfigDir                = "config"
+	FreeDir                  = "freeTasks"
+	Epoch                    = "epoch"
+	Status                   = "status"
+	TaskMaster               = "0"
+	NodeAddr                 = "address"
+	NodeTTL                  = "ttl"
+	Healthy                  = "healthy"
+	MapreduceWorkerStatusDir = "workStatus"
 )
 
 func EpochPath(appName string) string {
@@ -76,4 +77,13 @@ func MasterPath(job string) string {
 }
 func WorkerPath(job string, id uint64) string {
 	return path.Join("/", job, strconv.FormatUint(id, 10))
+}
+
+// etcd API for mapreduce based on old framework API
+func MapreduceWorkerStatusDir(appName string, id uint64) string {
+	return path.Join("/", appName, MapreduceWorkerStatusDir, strconv.FormatUint(id, 10))
+}
+
+func MapreduceWorkerStatusPath(appName string, id uint64, attr) string {
+	return path.Join(MapreduceWorkerStatusDir(appName, id), attr)
 }
